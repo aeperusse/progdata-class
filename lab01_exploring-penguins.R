0f90eed0c84ddec703a4e935a5934cadf743e4d0
aeperusse <aperusse@usf.edu>
2022-02-07 18:43:12 -0500

    Hopefully this fixes it

---
+++ lab01_exploring-penguins.R
@@ -0,0 +1,12 @@
+Lab1: Testing R Functions
+
+library(palmerpenguins)
+
+#Head shows the first parts of the simplified raw data of the package
+head(penguins)
+
+#Tail shows the last parts.
+tail(penguins)
+
+#This shows the structure of penguins
+str(penguins)
